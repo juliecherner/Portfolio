5d012b605caa02042aebf1647fd2ff90d16a7167
Julie 3 <juliechernen@gmail.com>
2023-08-29 12:51:03 +0300

    Animation is changed

--- app/_components/animation/index.tsx
+++ app/_components/animation/index.tsx
@@ -7,16 +7,18 @@ export default function Animation() {
       <div className="node-icon">
         <Image
           src="/images/icons8-nodejs-48.png"
-          width={50}
-          height={50}
+          width={100}
+          height={100}
           alt="⎔"
         />
       </div>
       <div id="animation-letters">
         <p id="animation-letter">B</p>
         <p id="animation-letter">E</p>
+        <p id="animation-letter">&nbsp;</p>
         <p id="animation-letter">I</p>
         <p id="animation-letter">N</p>
+        <p id="animation-letter">&nbsp;</p>
         <p id="animation-letter">T</p>
         <p id="animation-letter">O</p>
         <p id="animation-letter">U</p>
