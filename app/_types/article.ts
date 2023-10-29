21b56827d186b11f8f7a6725e15ea7222b5f9b9c
juliecherner <juliechernen@gmail.com>
2023-10-29 17:16:28 +0200

    UI is improved

--- app/_types/article.ts
+++ app/_types/article.ts
@@ -1,2 +1,1 @@
-// eslint-disable-next-line no-use-before-define
 export type Article = Record<string, string | [] | number | any>
--- app/_types/project.ts
+++ app/_types/project.ts
@@ -1,2 +1,1 @@
-// eslint-disable-next-line no-use-before-define
 export type Project = Record<string, string | [] | number | any>
